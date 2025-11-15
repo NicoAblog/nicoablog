0859c4259dfcf452ea0f5a2d832c338d64d364d4
NicoAblog <ncblg4@gmail.com>
2025-11-15 13:53:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("startBtn")?.addEventListener("click", function () {
+    alert("Welcome to CS Learn! Redirecting to Courses...");
+    window.location.href = "courses.html";
+});
+
